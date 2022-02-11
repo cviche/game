712468c44a869f037fd3142ff1179cc5d422e38f
Carlos <cviche@ucsc.edu>
2022-02-11 09:07:43 -0800

    Created a button component that will start the game and interact with the board service

---
+++ src/app/modules/gameboard/components/start-game-button/start-game-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-start-game-button',
+  templateUrl: './start-game-button.component.html',
+  styleUrls: ['./start-game-button.component.scss']
+})
+export class StartGameButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/gameboard/gameboard.module.ts
+++ src/app/modules/gameboard/gameboard.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './components/board/board.component';
+import { StartGameButtonComponent } from './components/start-game-button/start-game-button.component';
 
 
 
 @NgModule({
   declarations: [
-    BoardComponent
+    BoardComponent,
+    StartGameButtonComponent
   ],
   imports: [
     CommonModule
