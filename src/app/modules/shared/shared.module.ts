8b301af90fe23e49f9bd6744f352eee004d78425
Carlos <cviche@ucsc.edu>
2022-02-13 11:46:09 -0800

    Exported NavbarModule from SharedModule

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarModule } from './navbar/navbar.module';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavbarModule
   ]
 })
 export class SharedModule { }
