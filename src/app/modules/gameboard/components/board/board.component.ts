f6acd1291b9b74e7923a1047a8ee5301b6ba8972
Carlos <cviche@ucsc.edu>
2022-02-13 15:10:30 -0800

    Added logic to the BoardComponent

--- src/app/modules/gameboard/components/board/board.component.ts
+++ src/app/modules/gameboard/components/board/board.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
 
 @Component({
   selector: 'board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
-
-  constructor() { }
+export class BoardComponent implements OnInit, OnDestroy {
+  state: string = "initial";
+  constructor(private boardService : BoardService) { }
 
   ngOnInit(): void {
+    console.log("The board is being initialized...")
+    this.boardService.boardState.subscribe((state) => {
+      console.log(state);
+      this.state = state;
+    })
+  }
+  ngOnDestroy(): void {
+      console.log("The board is being destroyed...")
+  }
+
+  startBoard(): void {
+    this.boardService.startGame();
   }
 
 }
