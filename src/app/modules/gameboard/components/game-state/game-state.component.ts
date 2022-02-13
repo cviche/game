16e4ad6e7992372039f915765883aabfa3b0a9c7
Carlos <cviche@ucsc.edu>
2022-02-13 14:48:27 -0800

    Created a new component, GameState, that will contain the actual game that the user will be playing

---
+++ src/app/modules/gameboard/components/game-state/game-state.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-state',
+  templateUrl: './game-state.component.html',
+  styleUrls: ['./game-state.component.scss']
+})
+export class GameStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/gameboard/gameboard.module.ts
+++ src/app/modules/gameboard/gameboard.module.ts
@@ -4,12 +4,14 @@ import { BoardComponent } from './components/board/board.component';
 import { StartGameButtonComponent } from './components/start-game-button/start-game-button.component';
 import { GameboardRoutingModule } from './gameboard-routing.module';
 import { InitialStateComponent } from './components/initial-state/initial-state.component';
+import { GameStateComponent } from './components/game-state/game-state.component';
 
 @NgModule({
   declarations: [
     BoardComponent,
     StartGameButtonComponent,
     InitialStateComponent,
+    GameStateComponent,
   ],
   imports: [
     GameboardRoutingModule,
