6577e4fbe58982aa0c59b77b5ce0ccfaea18ae7c
Carlos <cviche@ucsc.edu>
2022-02-13 15:28:59 -0800

    BoardService can now start the game

--- src/app/modules/gameboard/services/board.service.ts
+++ src/app/modules/gameboard/services/board.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
+  boardState : Subject<string> = new Subject<string>();
+  constructor() {}
 
-  constructor() { }
+   startGame() : void {
+      this.boardState.next("in-progress");
+   }
 }
