9ce15a8cd32af8f2b36f881c0125abf0e7c24524
Carlos <cviche@ucsc.edu>
2022-02-14 10:31:52 -0800

    Added animations to the modal when it appears and disappears

--- src/app/modules/gameboard/components/modal/modal.component.ts
+++ src/app/modules/gameboard/components/modal/modal.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('void', style({opacity: 0})),
+      state('*', style({opacity: 1})),
+      transition(':enter', [animate('1s')]),
+      transition(':leave', [
+    animate('1400ms', style({ opacity: 0 }))
+  ])
+    ])
+  ]
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  constructor(private boardService : BoardService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  exitRules() {
+    this.boardService.exitRules();
   }
 
 }
