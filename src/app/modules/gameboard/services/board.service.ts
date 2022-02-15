d9a287e75c43ea8d3e0c8753eb2fee73af99337e
Carlos <cviche@ucsc.edu>
2022-02-14 16:36:57 -0800

    Added logic to the board service to keep track of the number of cards that are on the board

--- src/app/modules/gameboard/services/board.service.ts
+++ src/app/modules/gameboard/services/board.service.ts
@@ -6,9 +6,15 @@ import { Subject } from "rxjs";
 })
 export class BoardService {
   boardState : Subject<string> = new Subject<string>();
+  totalCards: number[] = [];
+  maxCards: number = 6;
+
   constructor() {}
 
    startGame() : void {
+      for(let i = 0; i < this.maxCards; i++) {
+        this.totalCards.push(i+1);
+      }
       this.boardState.next("in-progress");
    }
 
