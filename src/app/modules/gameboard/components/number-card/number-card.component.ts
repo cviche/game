5349cd6b1c1a91a248bcbe0d40d6256c807668bc
Carlos <cviche@ucsc.edu>
2022-02-15 07:11:59 -0800

    Added the fadeInOut animation to the NumberCard component

--- src/app/modules/gameboard/components/number-card/number-card.component.ts
+++ src/app/modules/gameboard/components/number-card/number-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BoardService } from '../../services/board.service';
-
+import {fadeInOut} from "../../animations/animations.animation";
 @Component({
   selector: 'number-card',
   templateUrl: './number-card.component.html',
-  styleUrls: ['./number-card.component.scss']
+  styleUrls: ['./number-card.component.scss'],
+  animations: fadeInOut
 })
 export class NumberCardComponent implements OnInit {
   @Input() value!: number;
