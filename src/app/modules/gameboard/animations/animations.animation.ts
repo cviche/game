89dd97ab71a466dc7052491c82b89e7d9a07b2ab
Carlos <cviche@ucsc.edu>
2022-02-16 17:13:08 -0800

    Created a new animation that removes the card from the board

--- src/app/modules/gameboard/animations/animations.animation.ts
+++ src/app/modules/gameboard/animations/animations.animation.ts
@@ -8,6 +8,13 @@ export const fadeInOut = [trigger('fadeInOut', [
       transition(':leave', [animate('250ms')])
     ])];
 
+export const cardRemoved = [trigger('cardRemoved', [
+      state('in', style({opacity: 1})),
+      state('out', style({opacity: 0})),
+      transition('in => out', [animate('250ms')]),
+      transition('out => in', [animate('250ms')])
+    ])];
+
 // This animation is needed if the component is destroyed and has a parent component
 // (Otherwise the animation when the component is destroyed will not be used).
 export const container = [trigger('container', [
